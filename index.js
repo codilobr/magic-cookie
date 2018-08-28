4696e818122d3c5a0bee689c7c962ebccccc8527
Caio Flores <caio@mytech.com.br>
2018-08-28 12:40:25 -0400

    Alteração da exportação das classes no index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const RedisStore = require('./lib/redis_store');
 const MemoryCookieStore = require('./lib/memstore');
 
-exports.RedisStore = RedisStore;
-exports.MemoryCookieStore = MemoryCookieStore;
+module.exports = { RedisStore, MemoryCookieStore };
